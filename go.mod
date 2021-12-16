5f1c41f20763df300ae3d2fb3b636631ca935cd8
mcfrappe <obeyfranso@gmail.com>
2021-12-16 20:54:04 +0100

    feat: added a button that lays itself out onto the window (all placed onto a context). Also using the material theme.

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module eggtimer
 
 go 1.17
 
+require gioui.org v0.0.0-20211215150519-af63c089f63c
+
 require (
-	gioui.org v0.0.0-20211215150519-af63c089f63c // indirect
 	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
 	gioui.org/shader v1.0.6 // indirect
+	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,38 @@ package main
 
 import (
     "gioui.org/app"
+    "gioui.org/unit"
+    "gioui.org/font/gofont"
+    "gioui.org/io/system"
+    "gioui.org/layout"
+    "gioui.org/op"
+    "gioui.org/widget"
+    "gioui.org/widget/material"
 )
 
 func main() {
     go func() {
         // create a new window
-        w := app.NewWindow()
+        window := app.NewWindow(
+            app.Title("Egg Timer"),
+            app.MaxSize(unit.Dp(400), unit.Dp(600)),
+            app.MinSize(unit.Dp(400), unit.Dp(600)),
+        )
+
+        // ops are for operations from the UI
+        var ops op.Ops
+        var startButton widget.Clickable
+        theme := material.NewTheme(gofont.Collection())
 
         // listen for events in the window
-        for range w.Events() {
+        for event := range window.Events() {
+            switch event := event.(type) {
+                case system.FrameEvent:
+                    graphical_ctx := layout.NewContext(&ops, event)
+                    button := material.Button(theme, &startButton, "Start")
+                    button.Layout(graphical_ctx)
+                    event.Frame(graphical_ctx.Ops)
+            }
         }
     }()
 
