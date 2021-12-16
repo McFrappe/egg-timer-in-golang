bb135c26837758e358e8d7d3944f9789cafb8aa4
mcfrappe <obeyfranso@gmail.com>
2021-12-16 21:09:12 +0100

    feat: added flexbox spacing

--- main.go
+++ main.go
@@ -29,10 +29,27 @@ func main() {
         for event := range window.Events() {
             switch event := event.(type) {
                 case system.FrameEvent:
+                    // Create a context to place objects onto
                     graphical_ctx := layout.NewContext(&ops, event)
-                    button := material.Button(theme, &startButton, "Start")
-                    button.Layout(graphical_ctx)
+
+                    layout.Flex{
+                        Axis: layout.Vertical,
+                        Spacing: layout.SpaceStart,
+                    }.Layout(graphical_ctx,
+                        layout.Rigid(
+                            // Create button with pointer from the clickable widget 'startButton'
+                            func(graphical_ctx layout.Context) layout.Dimensions {
+                                button := material.Button(theme, &startButton, "Start")
+                                return button.Layout(graphical_ctx)
+                            },
+                        ),
+                        layout.Rigid(
+                            layout.Spacer{Height: unit.Dp(30)}.Layout,
+                        ),
+                    )
+
                     event.Frame(graphical_ctx.Ops)
+
             }
         }
     }()
