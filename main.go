b621574d4bfa602fe32a0d1a8adaa5fdde934056
mcfrappe <obeyfranso@gmail.com>
2021-12-16 21:37:16 +0100

    chore: refactored code

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+    "log"
+    "os"
+    "image/color"
+
     "gioui.org/app"
     "gioui.org/unit"
     "gioui.org/font/gofont"
@@ -11,6 +15,57 @@ import (
     "gioui.org/widget/material"
 )
 
+type C = layout.Context
+type D = layout.Dimensions
+
+// rgba(11, 232, 129,1.0)
+var emeraldColor = color.NRGBA{
+    R: 255,
+    G: 232, 
+    B: 129, 
+    A: 1.0,
+}
+
+func draw(window *app.Window) error {
+    // ops are for operations from the UI
+    var ops op.Ops
+    var startButton widget.Clickable
+    theme := material.NewTheme(gofont.Collection())
+
+    // listen for events in the window
+    for event := range window.Events() {
+        switch event := event.(type) {
+            case system.FrameEvent:
+                // Create a context to place objects onto
+                graphical_ctx := layout.NewContext(&ops, event)
+
+                layout.Flex{
+                    Axis: layout.Vertical,
+                    Spacing: layout.SpaceStart,
+                }.Layout(graphical_ctx,
+                    layout.Rigid(
+                        // Create button with pointer from the clickable widget 'startButton'
+                        func(graphical_ctx C) D {
+                            button := material.Button(theme, &startButton, "Start")
+                            // button.Background = emeraldColor
+                            return button.Layout(graphical_ctx)
+                        },
+                    ),
+                    layout.Rigid(
+                        layout.Spacer{Height: unit.Dp(30)}.Layout,
+                    ),
+                )
+
+                event.Frame(graphical_ctx.Ops)
+
+            case system.DestroyEvent:
+                return event.Err
+        }
+    }
+
+    return nil
+}
+
 func main() {
     go func() {
         // create a new window
@@ -20,38 +75,11 @@ func main() {
             app.MinSize(unit.Dp(400), unit.Dp(600)),
         )
 
-        // ops are for operations from the UI
-        var ops op.Ops
-        var startButton widget.Clickable
-        theme := material.NewTheme(gofont.Collection())
-
-        // listen for events in the window
-        for event := range window.Events() {
-            switch event := event.(type) {
-                case system.FrameEvent:
-                    // Create a context to place objects onto
-                    graphical_ctx := layout.NewContext(&ops, event)
-
-                    layout.Flex{
-                        Axis: layout.Vertical,
-                        Spacing: layout.SpaceStart,
-                    }.Layout(graphical_ctx,
-                        layout.Rigid(
-                            // Create button with pointer from the clickable widget 'startButton'
-                            func(graphical_ctx layout.Context) layout.Dimensions {
-                                button := material.Button(theme, &startButton, "Start")
-                                return button.Layout(graphical_ctx)
-                            },
-                        ),
-                        layout.Rigid(
-                            layout.Spacer{Height: unit.Dp(30)}.Layout,
-                        ),
-                    )
-
-                    event.Frame(graphical_ctx.Ops)
-
-            }
+        if err := draw(window); err != nil {
+            log.Fatal(err)
         }
+
+        os.Exit(0)
     }()
 
     app.Main()
